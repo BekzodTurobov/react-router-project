c58a0eac680004f0fb0d3b005ec205293c01cc7d
BekzodTurobov <cosmos.4618282@gmail.com>
2023-08-07 21:52:00 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import EditPost from "./components/EditPost";
+import { useEffect } from "react";
+import useAxiosFetch from "./Hooks/useAxiosFetch";
+import { Route, Routes } from "react-router-dom";
+import { action, useStoreActions } from "easy-peasy";
 
 function App() {
+  const setPosts = useStoreActions((actions) => actions.setPosts);
+
+  const { data, fetchError, isLoading } = useAxiosFetch(
+    "http://localhost:3000/posts"
+  );
+
+  useEffect(() => {
+    setPosts(data);
+  }, [data, setPosts]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="React Js Blog" />
+      <Nav />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home path="/" isLoading={isLoading} fetchError={fetchError} />
+          }
+        ></Route>
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" Component={About} />
+        <Route path="*" Component={Missing} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
